90a937bb849d4768a321dad3d4c87dbd891b2746
VietHuy11 <90809862+VietHuy11@users.noreply.github.com>
2022-02-15 11:50:12 +0700

    AdjustResize in activity_main layout just work when RelativeLayout is parent layout , not work when ScrollView is. I need to use MotionLayout instead of setting adjustReisze. Must learn MotionLayout , and back to that later.

--- app/src/main/java/theintership/my/MainActivity.kt
+++ app/src/main/java/theintership/my/MainActivity.kt
@@ -24,51 +24,50 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN or WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
 
 
-        val btn_create_account = findViewById<TextView>(R.id.btn_signin_createAccout)
-        val edt_signin_account = findViewById<EditText>(R.id.edt_signin_account)
-        val edt_signin_password = findViewById<EditText>(R.id.edt_signin_password)
-        val icon_password_nosee = findViewById<ImageView>(R.id.password_nosee)
-        val icon_password_see = findViewById<ImageView>(R.id.password_see)
-        val btn_signin = findViewById<TextView>(R.id.btn_signin_go)
-        scrollView = findViewById<ScrollView>(R.id.layout_main_activity)
-
-
-        edt_signin_password.doAfterTextChanged {
-            val password = edt_signin_password.text
-            if (!password.isEmpty()) {
-                icon_password_see.visibility = View.VISIBLE
-            } else {
-                icon_password_see.visibility = View.INVISIBLE
-                icon_password_nosee.visibility = View.INVISIBLE
-            }
-        }
-
-        edt_signin_account.setOnClickListener {
-            handleShiftUp(edt_signin_account)
-            //AndroidBug5497Workaround().assistActivity(this)
-        }
-
-        icon_password_see.setOnClickListener {
-            if (icon_password_nosee.visibility == View.VISIBLE) {
-                icon_password_nosee.visibility = View.INVISIBLE
-                edt_signin_password.transformationMethod =
-                    PasswordTransformationMethod.getInstance()
-                edt_signin_password.setSelection(edt_signin_password.length())
-            } else {
-                icon_password_nosee.visibility = View.VISIBLE
-                edt_signin_password.transformationMethod =
-                    HideReturnsTransformationMethod.getInstance()
-                edt_signin_password.setSelection(edt_signin_password.length())
-            }
-        }
-
-        btn_create_account.setOnClickListener {
-            startActivity(Intent(this, Signup1Activity::class.java))
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
+//        val btn_create_account = findViewById<TextView>(R.id.btn_signin_createAccout)
+//        val edt_signin_account = findViewById<EditText>(R.id.edt_signin_account)
+//        val edt_signin_password = findViewById<EditText>(R.id.edt_signin_password)
+//        val icon_password_nosee = findViewById<ImageView>(R.id.password_nosee)
+//        val icon_password_see = findViewById<ImageView>(R.id.password_see)
+//        val btn_signin = findViewById<TextView>(R.id.btn_signin_go)
+//        scrollView = findViewById<ScrollView>(R.id.layout_main_activity)
+//
+//
+//        edt_signin_password.doAfterTextChanged {
+//            val password = edt_signin_password.text
+//            if (!password.isEmpty()) {
+//                icon_password_see.visibility = View.VISIBLE
+//            } else {
+//                icon_password_see.visibility = View.INVISIBLE
+//                icon_password_nosee.visibility = View.INVISIBLE
+//            }
+//        }
+//
+//        edt_signin_account.setOnClickListener {
+//            //handleShiftUp(edt_signin_account)
+//            //AndroidBug5497Workaround().assistActivity(this)
+//        }
+//
+//        icon_password_see.setOnClickListener {
+//            if (icon_password_nosee.visibility == View.VISIBLE) {
+//                icon_password_nosee.visibility = View.INVISIBLE
+//                edt_signin_password.transformationMethod =
+//                    PasswordTransformationMethod.getInstance()
+//                edt_signin_password.setSelection(edt_signin_password.length())
+//            } else {
+//                icon_password_nosee.visibility = View.VISIBLE
+//                edt_signin_password.transformationMethod =
+//                    HideReturnsTransformationMethod.getInstance()
+//                edt_signin_password.setSelection(edt_signin_password.length())
+//            }
+//        }
+//
+//        btn_create_account.setOnClickListener {
+//            startActivity(Intent(this, Signup1Activity::class.java))
+//            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+//        }
 
     }
     fun handleShiftUp(focusedView: View?) {
