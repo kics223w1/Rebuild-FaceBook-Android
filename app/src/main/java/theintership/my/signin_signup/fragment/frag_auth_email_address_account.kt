1e6f9f8de6d0035c751f75ccb0882d3d3035892e
laptop <caoviethuy11@gmail.com>
2022-03-20 01:46:10 +0700

    Done adding Coroutine when authencation email. Not done in authencation phone number , add again2.

--- app/src/main/java/theintership/my/signin_signup/fragment/frag_auth_email_address_account.kt
+++ app/src/main/java/theintership/my/signin_signup/fragment/frag_auth_email_address_account.kt
@@ -74,6 +74,7 @@ class frag_auth_email_address_account : Fragment(R.layout.frag_auth_email_addres
             //If use the old one , it will not set isEmailVerified on time
             if (mUser != null) {
                 if (!mUser.isEmailVerified) {
+                    //OK
                     println("debug mUser email: ${mUser.email.toString()}")
                     val s = "If you have verified , please click again. Or please verify the eamil."
                     s.showToastShort(signup1activity)
